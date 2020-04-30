06523e4d264a88b547c75089b869a78219d3ac9f
Naman Jain <namanjain0501@gmail.com>
2020-04-30 20:11:06 +0530

    added search on top

--- appointment/models.py
+++ appointment/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+status_choices =(
+    ('pending','pending'),
+    ('approved','approved'),
+    ('rejected','rejected'),
+    ('cancelled','cancelled'),
+)
+
+class appointment(models.Model):
+    doc = models.ForeignKey(User,null=True, on_delete=models.CASCADE,related_name="ap1")
+    pat = models.ForeignKey(User,null=True, on_delete=models.CASCADE,related_name="ap2")
+
+    date_time = models.CharField(max_length = 100)
+    status = models.CharField(max_length = 20 ,default='rejected',choices=status_choices)
+    doc_cmts = models.CharField(max_length=150)
+
