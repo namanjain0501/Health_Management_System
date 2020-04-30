a9d98043105e3553c1fd90aa484f47a5af57c411
Naman Jain <namanjain0501@gmail.com>
2020-05-01 01:47:03 +0530

    host added

--- health/urls.py
+++ health/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 from accounts import views as acc_views
 
@@ -39,4 +40,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns+= staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
